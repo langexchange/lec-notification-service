a364c31fa8571e57fe8b989021b73e3774920408
Nguyễn Văn Thương <nvthuong1508@gmail.com>
2023-03-29 14:04:04 +0700

    update

--- src/le-notification-service/LE.NotificationService/Dtos/NotificationDto.cs
+++ src/le-notification-service/LE.NotificationService/Dtos/NotificationDto.cs
@@ -1,6 +1,14 @@
-﻿namespace LE.NotificationService.Dtos
+﻿using System;
+
+namespace LE.NotificationService.Dtos
 {
     public class NotificationDto
     {
+        public Guid Notiid { get; set; }
+        public Guid Boxid { get; set; }
+        public string Text { get; set; }
+        public string Url { get; set; }
+        public Guid Type { get; set; }
+
     }
 }
